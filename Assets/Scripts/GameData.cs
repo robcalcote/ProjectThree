c353b38cd794792dba14f8a32371bddb6a19cbad
BangoJetty <bango.calcote@gmail.com>
2025-01-28 16:38:10 -0700

    Saving progress

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+    public List<GameObject> enemies;
+    public List<GameObject> maps;
+    public List<GameObject> towers;
+}
