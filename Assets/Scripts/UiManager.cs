12a495a36e2e286a06c6f739064fb14062267ce1
Rob <robcalcote@gmail.com>
2025-02-01 15:42:25 -0700

    added PlacementGrid

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -4,11 +4,19 @@ public class UiManager : MonoBehaviour
 {
     GameData gameData;
     public GameObject selectedTower;
+    public bool towerPlacementMode = false;
+    public GameObject placementGrid;
+    
+    public void toggleTowerPlacementMode()
+    {
+        towerPlacementMode = !towerPlacementMode;
+        placementGrid.SetActive(towerPlacementMode);
+    }
     
     public void SelectTower(int towerIndex)
     {
         selectedTower = gameData.towers[towerIndex];
-        Debug.Log(selectedTower);
+        toggleTowerPlacementMode();
     }
 
     void Start()
